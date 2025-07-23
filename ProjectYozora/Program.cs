01ae3bef1c7d10b6dde356cd9e9b4c9f509f8706
Mira <56395159+TheXorog@users.noreply.github.com>
2025-07-23 21:15:57 +0200

    Initial commit

---
+++ ProjectYozora/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectYozora;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ProjectYozora
+Yozora is a modular, feature packed discord bot. This monorepo contains all the different bots.
